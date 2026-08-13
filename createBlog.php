63aedfd75e3682b8b6e1b4c214e0000d8f5acd9f
Ashani Chathurya <rathnasiriwaac.24@uom.lk>
2026-08-14 00:52:34 +0530

    Add blog creation feature

--- createBlog.php
+++ createBlog.php
@@ -1,4 +1,60 @@
 <?php
+
 include "authen.php";
 include "database.php";
+
+if (isset($_POST['create'])) {
+
+    $title = $_POST['title'];
+    $content = $_POST['content'];
+    $user_id = $_SESSION['user_id'];
+
+    $stmt = mysqli_prepare(
+        $conn,
+        "INSERT INTO blogPost (user_id, title, content) VALUES (?, ?, ?)"
+    );
+
+    mysqli_stmt_bind_param($stmt, "iss", $user_id, $title, $content);
+
+    if (mysqli_stmt_execute($stmt)) {
+        header("Location: index.php");
+        exit();
+    } else {
+        $error = "Error creating blog: " . mysqli_error($conn);
+    }
+
+    mysqli_stmt_close($stmt);
+}
+
 ?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Create Blog</title>
+</head>
+
+<body>
+
+    <h2>Create New Blog</h2>
+
+    <?php
+    if (isset($error)) {
+        echo "<p>$error</p>";
+    }
+    ?>
+
+    <form action="" method="POST">
+
+        <label>Blog Title</label><br>
+        <input type="text" name="title" required><br><br>
+
+        <label>Blog Content</label><br>
+        <textarea name="content" rows="10" cols="50" required></textarea><br><br>
+
+        <button type="submit" name="create">Create Blog</button>
+
+    </form>
+
+</body>
+</html>
